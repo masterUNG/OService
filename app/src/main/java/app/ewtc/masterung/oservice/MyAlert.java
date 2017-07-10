c626c40011fd0987c3875f772166246f8938f5ba
MasterUNG <phrombutr@gmail.com>
2017-07-10 16:05:05 +0700

    Finish

---
+++ app/src/main/java/app/ewtc/masterung/oservice/MyAlert.java
@@ -0,0 +1,36 @@
+package app.ewtc.masterung.oservice;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by masterung on 7/10/2017 AD.
+ */
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_name);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+}   // Main Class
--- app/src/main/java/app/ewtc/masterung/oservice/RegisterFragment.java
+++ app/src/main/java/app/ewtc/masterung/oservice/RegisterFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by masterung on 7/10/2017 AD.
@@ -30,4 +32,67 @@ public class RegisterFragment extends Fragment{
 
         return view;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        backController();
+
+        //Save Controller
+        saveController();
+
+
+    }
+
+    private void saveController() {
+
+        //Initial View
+        ImageView imageView = (ImageView) getView().findViewById(R.id.imvSave);
+        final EditText nameEditText = (EditText) getView().findViewById(R.id.edtName);
+        final EditText userEditText = (EditText) getView().findViewById(R.id.edtUser);
+        final EditText passwordEditText = (EditText) getView().findViewById(R.id.edtPassword);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                String strName = nameEditText.getText().toString().trim();
+                String strUser = userEditText.getText().toString().trim();
+                String strPassword = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (strName.equals("") || strUser.equals("") || strPassword.equals("")) {
+                    //Have Space
+
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.have_space),
+                            getString(R.string.message_have_space));
+
+                } else {
+                    //Have No Space
+                }
+
+            }
+        });
+
+    }
+
+    private void backController() {
+        ImageView imageView = (ImageView) getView().findViewById(R.id.imvBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainContent, MainFragment.mainInstant())
+                        .commit();
+
+            }
+        });
+    }
+
 }   // Main Class
